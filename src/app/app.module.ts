07820c74bce4be36455ff35c624e2f31991ad945
Yusuf Benjamin <yusuf.benjamin@outlook.com>
2023-02-23 14:07:44 +0200

    landing: Generate boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './presentational/card/card.component';
+import { LandingComponent } from './container/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
