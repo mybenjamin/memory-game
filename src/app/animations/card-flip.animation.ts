be79e8b7361ea4de44e65ef5c8b96633151fbf18
Yusuf Benjamin <yusuf.benjamin@outlook.com>
2023-02-23 18:57:29 +0200

    eslint: Enforce single quotes

--- src/app/animations/card-flip.animation.ts
+++ src/app/animations/card-flip.animation.ts
@@ -1,23 +1,23 @@
-import { trigger, state, style, transition, animate } from "@angular/animations";
-import { CardStateEnum } from "../models/card-state.enum";
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { CardStateEnum } from '../models/card-state.enum';
 
 const normalToFlipped = (): string => `${CardStateEnum.flipped} => ${CardStateEnum.normal}`;
 const flippedToNormal = (): string => `${CardStateEnum.normal} => ${CardStateEnum.flipped}`;
 
 export const cardFlip =
-    trigger("cardFlip", [
+    trigger('cardFlip', [
         state(
             CardStateEnum.normal,
             style({
-                transform: "none"
+                transform: 'none'
             })
         ),
         state(
             CardStateEnum.flipped,
             style({
-                transform: "rotateY(180deg)"
+                transform: 'rotateY(180deg)'
             })
         ),
-        transition(normalToFlipped(), [animate("400ms")]),
-        transition(flippedToNormal(), [animate("400ms")]),
+        transition(normalToFlipped(), [animate('400ms')]),
+        transition(flippedToNormal(), [animate('400ms')]),
     ]);
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'memory-game'`, () => {
+  it('should have as title memory-game', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('memory-game');
