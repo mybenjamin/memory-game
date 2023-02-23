17529c17f4a84fe59550a0164d047996725b60ab
Yusuf Benjamin <yusuf.benjamin@outlook.com>
2023-02-23 18:33:14 +0200

    card: Add card flip animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
+// Angular Imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Material Imports
+import {MatCardModule} from '@angular/material/card';
+
+// App Imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './presentational/card/card.component';
 import { LandingComponent } from './container/landing/landing.component';
 
@@ -15,8 +20,9 @@ import { LandingComponent } from './container/landing/landing.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/presentational/card/card.component.ts
+++ src/app/presentational/card/card.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { CardStateEnum } from 'src/app/models/card-state.enum';
+import { cardFlip } from '../../animations/card-flip.animation';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  animations: [cardFlip]
 })
 export class CardComponent {
+  state = CardStateEnum.normal;
 
+  onCardClick(): void {
+    if (this.state !== CardStateEnum.normal) {
+      this.state = CardStateEnum.normal;
+      return;
+    }
+    this.state = CardStateEnum.flipped;
+  }
 }
